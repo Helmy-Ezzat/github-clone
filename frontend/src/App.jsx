8c2b857604d4427d3ac70b6c4c34e0d4961e6b84
Helmy Ezzat <hellmyezzat@gmail.com>
2024-02-24 08:38:19 +0200

    "Configured routing using React Router. Created routes for Home, Login, Sign Up, Explore, and Likes pages."

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home, Explore, Likes, SignUp, Login } from './pages'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/likes" element={<Likes />} />
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home'
+export { default as Explore } from './Explore'
+export { default as Likes } from './Likes'
+export { default as SignUp } from './SignUp'
+export { default as Login } from './Login'
